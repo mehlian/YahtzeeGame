f0e343aa4b65ec3fd612744b525c60f4e693a95f
mehlian <xpilarz0@gmail.com>
2018-01-02 19:17:03 +0100

    done Table class and TableTests;

---
+++ Yahtzee.UnitTests/GameTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Yahtzee.Core;
+
+namespace Yahtzee.UnitTests
+{
+    [TestFixture]
+    public class GameTests
+    {
+        [Test]
+        public void Game_CanBeInitialized()
+        {
+            Game game = new Game();
+        }
+
+        [Test]
+        public void MethodToTest()
+        {
+
+        }
+    }
+}
