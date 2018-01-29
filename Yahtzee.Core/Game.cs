11327b572f140349d984206a9f927b329a5cbee9
mehlian <xpilarz0@gmail.com>
2018-01-29 12:42:27 +0100

    end of addPlayer method;

---
+++ Yahtzee.Core/Game.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Yahtzee.Core
+{
+    public class Game
+    {
+        public List<string> Players { get; protected set; }
+        public uint PlayerNameLength { get; set; } = 10;
+
+        public Game()
+        {
+            Players = new List<string>();
+        }
+
+        public void AddPlayer(string playerName)
+        {
+            if (playerName.Length > PlayerNameLength)
+            {
+                throw new ArgumentException($"Max player name length is {PlayerNameLength}.");
+            }
+            if (Players.Count >= 4)
+            {
+                throw new ArgumentException($"Max number of players allowed: 4.");
+            }
+            Players.Add(playerName);
+        }
+    }
+}
---
+++ Yahtzee.UnitTests/GameTests.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using Yahtzee.Core;
+
+namespace Yahtzee.UnitTests
+{
+    [TestFixture]
+    public class GameTests
+    {
+        private Game game;
+
+        [SetUp]
+        public void SetUp()
+        {
+            game = new Game();
+        }
+
+        [Test]
+        public void Game_CanBeCreated()
+        {
+            Game game = new Game();
+        }
+
+        [Test]
+        public void AddPlayer_PlayerNameAsString_PlayerIsAdded()
+        {
+            game.AddPlayer("A");
+            var result = game.Players;
+            var expected = new List<string> { "A" };
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void PlayerNameLength_DefaultPlayerNameLength_Returns10()
+        {
+            Assert.AreEqual(10, game.PlayerNameLength);
+        }
+
+        [Test]
+        public void AddPlayer_PlayerNameIsLongerThanAllowed_ThrowsException()
+        {
+            game.PlayerNameLength = 10;
+            TestDelegate result = () => game.AddPlayer("01234567890");
+
+            Assert.Throws<ArgumentException>(result);
+        }
+
+        [Test]
+        public void AddPlayer_5thPlayerIsAdded_ThrowsException()
+        {
+            game.AddPlayer("1");
+            game.AddPlayer("2");
+            game.AddPlayer("3");
+            game.AddPlayer("4");
+            TestDelegate result = () => game.AddPlayer("5");
+
+            Assert.Throws<ArgumentException>(result);
+        }
+    }
+}
