d59f7396f0b82d24aa9d4015ac8f2da5b14d7b63
mehlian <xpilarz0@gmail.com>
2018-02-05 09:36:21 +0100

    Added RollDice logic to Game class;

--- Yahtzee.Core/Game.cs
+++ Yahtzee.Core/Game.cs
@@ -1,9 +1,30 @@
-﻿namespace Yahtzee.Core
+﻿using System;
+using System.Collections.Generic;
+
+namespace Yahtzee.Core
 {
     public class Game
     {
-        public Game()
+        private const int MIN_VALUE = 1;
+        private const int MAX_VALUE = 6;
+        private IRandomizer _randomizer;
+
+        public Game(IRandomizer randomizer)
         {
+            _randomizer = randomizer;
+        }
+
+        public List<Dice> RollDice(List<Dice> dice)
+        {
+            foreach (Dice die in dice)
+            {
+                if (die.IsUnlocked)
+                {
+                    die.Result = _randomizer.Roll(MIN_VALUE, MAX_VALUE);
+                }
+            }
+
+            return dice;
         }
     }
 }
--- Yahtzee.UnitTests/GameTests.cs
+++ Yahtzee.UnitTests/GameTests.cs
@@ -1,22 +1,74 @@
 ﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
 using Yahtzee.Core;
+using NSubstitute;
 
 namespace Yahtzee.UnitTests
 {
     [TestFixture]
     public class GameTests
     {
+        private Game _game;
+        private IRandomizer _randomizer;
+
+        [SetUp]
+        public void Setup()
+        {
+            _randomizer = Substitute.For<IRandomizer>();
+            _game = new Game(_randomizer);
+        }
 
         [Test]
         public void Game_CanBeCreated()
         {
-            Game game = new Game();
+            Game game = new Game(_randomizer);
         }
 
         [Test]
-        public void RollDice_()
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        [TestCase(6)]
+        public void RollDice_GivenListOfDicesThatAreUnlocked_ReturnsListOfDicesWithNewResults(int expected)
         {
+            _randomizer.Roll(1, 6).Returns(expected);
+            List<Dice> dices = new List<Dice>
+            {
+                new Dice(),
+                new Dice(),
+                new Dice(),
+                new Dice(),
+                new Dice()
+            };
 
+            List<Dice> result = _game.RollDice(dices);
+
+            Assert.That(result.Any(d => d.Result == expected));
         }
+
+        [Test]
+        public void RollDice_GivenListOfDicesThatAreLocked_ReturnsListOfDicesWithOldResults()
+        {
+            _randomizer.Roll(1, 6).Returns(1);
+            List<Dice> dices = new List<Dice>
+            {
+                new Dice(),
+                new Dice(),
+                new Dice(),
+                new Dice(),
+                new Dice()
+            };
+
+            _game.RollDice(dices);
+            dices.ForEach(d => d.Lock());
+            _randomizer.Roll(1, 6).Returns(2);
+            List<Dice> result = _game.RollDice(dices);
+
+            Assert.IsTrue(result.Any(d => d.Result == 1));
+        }
+
     }
 }
