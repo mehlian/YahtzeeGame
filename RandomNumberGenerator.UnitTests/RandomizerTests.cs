2b119d1975160af8af366ae51c51862f55766e0c
mehlian <xpilarz0@gmail.com>
2018-02-19 17:17:46 +0100

    Added RandomNumberGenerator and RandomNumberGenerator.UnitTests project;

---
+++ RandomNumberGenerator.UnitTests/RandomizerTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yahtzee.Core;
+
+namespace RandomNumberGenerator.UnitTests
+{
+    [TestFixture]
+    public class RandomizerTests
+    {
+        [Test]
+        public void Randomizer_CanBeCreated()
+        {
+            Randomizer randomizer = new Randomizer();
+        }
+
+        [Test]
+        public void Randomizer_ImplementsIRandomizer()
+        {
+            Randomizer randomizer = new Randomizer();
+
+            Assert.IsInstanceOf<IRandomizer>(randomizer);
+        }
+    }
+}
---
+++ RandomNumberGenerator/Randomizer.cs
@@ -0,0 +1,12 @@
+﻿using Yahtzee.Core;
+
+namespace RandomNumberGenerator
+{
+    public class Randomizer : IRandomizer
+    {
+        public int GetRandomNumber(int minValue, int maxValue)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
