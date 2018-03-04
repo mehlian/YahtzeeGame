cab840e80be55e45cc317eeb5f0fe8df2765e425
mehlian <xpilarz0@gmail.com>
2018-03-04 11:17:15 +0100

    Added Yahtzee.WinClient.UnitTests project; added BoolToColorConverter and it's tests; hooked up converter in GameWindow.xaml;

---
+++ Yahtzee.WinClient.UnitTests/BoolToColoRConverterTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Yahtzee.WinClient.UnitTests
+{
+    [TestFixture]
+    public class BoolToColoRConverterTests
+    {
+        [Test]
+        public void BoolToColorConverter_CanBeCreated()
+        {
+            BoolToColorConverter converter = new BoolToColorConverter();
+        }
+
+        [Test]
+        public void BoolToColorConverter_ImplementsIValueConverter()
+        {
+            BoolToColorConverter converter = new BoolToColorConverter();
+
+            Assert.IsInstanceOf<IValueConverter>(converter);
+        }
+
+        [Test]
+        public void Convert_FalseAsValue_ReturnsTransparentBrush()
+        {
+            BoolToColorConverter converter = new BoolToColorConverter();
+
+            var expected = new SolidColorBrush(Color.FromArgb(0, 255, 255, 210)).Color;
+            var result = ((SolidColorBrush)converter.Convert(false, null, null, null)).Color;
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Convert_TrueAsValue_ReturnsLightGoldenrodYellowBrush()
+        {
+            BoolToColorConverter converter = new BoolToColorConverter();
+
+            var expected = new SolidColorBrush(Color.FromArgb(255, 255, 255, 210)).Color;
+            var result = ((SolidColorBrush)converter.Convert(true, null, null, null)).Color;
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ Yahtzee.WinClient/BoolToColorCoverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Yahtzee.WinClient
+{
+    public class BoolToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value == true ? new SolidColorBrush(Color.FromArgb(255, 255, 255, 210)) : new SolidColorBrush(Color.FromArgb(0, 255, 255, 210));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
